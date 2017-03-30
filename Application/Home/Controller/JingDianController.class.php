56f57acfa6eaf4b7ebd8efcdf84f7e0c11f9e3b9
mali <mali1711@163.com>
2017-03-30 15:16:51 +0800

    baidu map api url

---
+++ Application/Home/Controller/JingDianController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class JingDianController extends Controller {
+
+    public function addJingDian()
+    {
+        
+    }
+
+    public function show()
+    {
+        var_dump($_GET);
+    }
+    
+}
